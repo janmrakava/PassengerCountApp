3698125cb3b920c722b10d15b9a092db0ba58edb
janmrakava <janmrakava@gmail.com>
2022-02-19 18:30:41 +0100

    Tlačítko SAVE, SNÍŽENÍ a vypisování hodnot do stringu.

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 let count = 0;
 let countEl = document.getElementById("count-el");
 
+let saveEl = document.getElementById("save-el");
+
+
 function increment(){
   count+=1;
-  countEl.innerText = count;
+  countEl.textContent = count;
+}
+
+function decrement(){
+  count-=1;
+  countEl.textContent = count;
 }
 
 function save() {
-  console.log(count);
-
+  let text = count + " -";
+  saveEl.textContent += text;
+  count = 0;
+  countEl.textContent = count;
 }
