00238144a14fb8cca5586fadb8a94f327fb812b3
janmrakava <janmrakava@gmail.com>
2022-02-19 17:24:49 +0100

    Tlačítko save

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let count = 0;
 let countEl = document.getElementById("count-el");
 
-console.log(countEl);
-
-
 function increment(){
   count+=1;
   countEl.innerText = count;
+}
+
+function save() {
   console.log(count);
+
 }
